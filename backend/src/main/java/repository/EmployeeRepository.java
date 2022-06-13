29c9d83b0f7b49e204868c089c5aa2202a46e7cf
Ernestas <ernestas.urbonas57@gmail.com>
2022-06-13 19:18:50 +0300

    make backend create table in database

---
+++ backend/src/main/java/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+		// all crud database methods
+}
